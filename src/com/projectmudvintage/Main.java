ea76093024b1cfd8888b53bd879de2d6ae0af6aa
Braiant Malta <braiant.malta@alunos.unis.edu.br>
2021-08-17 21:05:51 -0300

    Initializing :)

--- src/com/projectmudvintage/Main.java
+++ src/com/projectmudvintage/Main.java
@@ -1,8 +1,24 @@
+/**
+ * @author Braioso
+ */
 package com.projectmudvintage;
 
 public class Main {
-
     public static void main(String[] args) {
-	System.out.println("Starting...");
+        Sala room = new Sala("Templo de Midgard", "A very huge temple with a monumental found in the center that you can see at right a donation room.","N, S, E, W");
+        room.showSala();
+    }
+}
+class Sala {
+    String name, description, direction;
+
+    public Sala(){};
+    public Sala(String name, String description, String direction){
+        this.name = name;
+        this.description = description;
+        this.direction = direction;
+    }
+    public void showSala(){
+        System.out.printf("Room Name: %s\nDescriptions: %s\nDirections: [%s]", this.name, this.description, this.direction);
     }
 }
